1e3e70398d0f7838900f10ea5162420a3446d921
深空 <2793500992@qq.com>
2025-04-06 20:59:46 -0400

    Fix: Fix call the anthropic provider endpoint failing
    Please migrate from pnpm back to npm due to the anthropic SDK not support pnpm builtin fetch function. It can only use the node-fetch libraries which conflict with Electron in pnpm environment.

--- src/services/providers/anthropic-service.ts
+++ src/services/providers/anthropic-service.ts
@@ -1,12 +1,13 @@
-import { AnthropicProvider, createAnthropic } from '@ai-sdk/anthropic';
-import { Message } from '../../types/chat';
+import Anthropic from '@anthropic-ai/sdk';
+import { Message, MessageRole } from '../../types/chat';
 import { AiServiceProvider, CompletionOptions } from '../core/ai-service-provider';
 import { StreamControlHandler } from '../streaming-control';
-import { CommonProviderHelper } from './common-provider-service';
 import { SettingsService } from '../settings-service';
 import { mapModelCapabilities } from '../../types/capabilities';
 import { AIServiceCapability } from '../../types/capabilities';
 import { ModelSettings } from '../../types/settings';
+import { LanguageModelUsage } from 'ai';
+import { v4 as uuidv4 } from 'uuid';
 
 export const ANTHROPIC_PROVIDER_NAME = 'Anthropic';
 
@@ -16,7 +17,7 @@ export const ANTHROPIC_PROVIDER_NAME = 'Anthropic';
 export class AnthropicService implements AiServiceProvider {
   private settingsService: SettingsService;
   private _apiKey: string = '';
-  private ProviderInstance: AnthropicProvider;
+  private anthropic: Anthropic;
 
   private apiModels: ModelSettings[] = [];
 
@@ -29,11 +30,12 @@ export class AnthropicService implements AiServiceProvider {
     
     this._apiKey = providerSettings.apiKey || '';
 
-    this.ProviderInstance = createAnthropic({
+    this.anthropic = new Anthropic({
       apiKey: this._apiKey,
-      headers: {
-        "anthropic-dangerous-direct-browser-access": "true",
-      }
+      defaultHeaders: {
+        'anthropic-beta': 'output-128k-2025-02-19'
+      },
+      dangerouslyAllowBrowser: true,
     });
   }
 
@@ -96,8 +98,9 @@ export class AnthropicService implements AiServiceProvider {
    * Setup authentication for OpenAI
    */
   public recreateClient(): void {
-    this.ProviderInstance = createAnthropic({
-      apiKey: this._apiKey
+    this.anthropic = new Anthropic({
+      apiKey: this._apiKey,
+      dangerouslyAllowBrowser: true,
     });
   }
 
@@ -116,8 +119,132 @@ export class AnthropicService implements AiServiceProvider {
     options: CompletionOptions,
     streamController: StreamControlHandler
   ): Promise<Message> {
-    const modelInstance = this.ProviderInstance.languageModel(options.model);
-    return CommonProviderHelper.getChatCompletionByModel(modelInstance, messages, options, streamController);
+
+    if(options.stream){
+      return this.streamText(messages, options, streamController);
+    }
+
+    return this.generateText(messages, options, streamController);
+  }
+
+  private async generateText(
+    messages: Message[],
+    options: CompletionOptions,
+    streamController: StreamControlHandler
+  ){
+    const formattedMessages: Anthropic.MessageParam[] = messages.map((message) => ({
+      role: message.role === 'system' ? 'user' : message.role as Anthropic.MessageParam['role'],
+      content: message.content as Anthropic.MessageParam['content'],
+    }));
+    
+    const response = await this.anthropic.messages.create({
+      model: options.model,
+      max_tokens: options.max_tokens || 2048,
+      stream: options.stream,
+      messages: formattedMessages
+    }) as Anthropic.Message;
+
+    if(response.content[0].type === 'text'){
+      const fullText = response.content[0].text;
+      streamController.onChunk(fullText);
+
+      const usage: LanguageModelUsage = {
+        promptTokens: response.usage.input_tokens,
+        completionTokens: response.usage.output_tokens,
+        totalTokens: response.usage.input_tokens + response.usage.output_tokens,
+      };
+      streamController.onFinish(usage);
+      
+      return {
+        messageId: uuidv4(),
+        conversationId: messages[0].conversationId,
+        role: 'assistant' as MessageRole,
+        content: fullText,
+        timestamp: new Date(),
+        provider: options.provider,
+        model: options.model,
+        tokens: 0,
+        fatherMessageId: null,
+        childrenMessageIds: [],
+        preferIndex: 0
+      };
+    }
+
+    throw new Error('Anthropic response is not a text');
+  }
+
+  private async streamText(
+    messages: Message[],
+    options: CompletionOptions,
+    streamController: StreamControlHandler
+  ){
+    const formattedMessages: Anthropic.MessageParam[] = messages.map((message) => ({
+      role: message.role === 'system' ? 'user' : message.role as Anthropic.MessageParam['role'],
+      content: message.content as Anthropic.MessageParam['content'],
+    }));
+
+    let fullText = '';
+
+    //I want to put the stream in a returnable promise
+    const streamPromise = new Promise<Message>((resolve, reject) => {
+
+      const stream = this.anthropic.messages.stream({
+        model: options.model,
+        max_tokens: options.max_tokens || 2048,
+        stream: options.stream,
+        messages: formattedMessages,
+      }).on('text', (text) => {
+        fullText += text;
+        streamController.onChunk(fullText);
+      }).on('error', (error) => {
+        reject(error);
+      }).on('finalMessage', (message) => {
+        const usage: LanguageModelUsage = {
+          promptTokens: message.usage.input_tokens,
+          completionTokens: message.usage.output_tokens,
+          totalTokens: message.usage.input_tokens + message.usage.output_tokens,
+        };
+        streamController.onFinish(usage);
+
+        const aiResponse: Message = {
+          messageId: uuidv4(),
+          conversationId: messages[0].conversationId,
+          role: 'assistant' as MessageRole,
+          content: fullText,
+          timestamp: new Date(),
+          provider: options.provider,
+          model: options.model,
+          tokens: 0,
+          fatherMessageId: null,
+          childrenMessageIds: [],
+          preferIndex: 0
+        };
+
+        resolve(aiResponse);
+      }).on('abort', () => {
+        const aiResponse: Message = {
+          messageId: uuidv4(),
+          conversationId: messages[0].conversationId,
+          role: 'assistant' as MessageRole,
+          content: fullText,
+          timestamp: new Date(),
+          provider: options.provider,
+          model: options.model,
+          tokens: 0,
+          fatherMessageId: null,
+          childrenMessageIds: [],
+          preferIndex: 0
+        };
+
+        resolve(aiResponse);
+      });
+
+      streamController.getAbortSignal().addEventListener('abort', () => {
+        stream.abort();
+      });
+    });
+
+    return streamPromise;
   }
 
   /**
